32d8efd9f6e731b82b0e8bd3fa18e56669cf3a88
d4nilevi4 <d4nia14@gmail.com>
2026-01-03 09:18:45 +0300

    Update Consumer's Components

--- src/Leontitas/Consumer/Components.cs
+++ src/Leontitas/Consumer/Components.cs
@@ -4,3 +4,13 @@ namespace Consumer;
 
 [Game] public struct Id : IComponent { public int Value; }
 [Game] public struct Hp : IComponent { public int Value; }
+[Game] public struct Alive : IComponent { }
+
+[Game]
+public struct Quaternion : IComponent
+{
+    public float X;
+    public float Y;
+    public float Z;
+    public float W;
+}
